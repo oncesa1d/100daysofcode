803f23d8aa9e3c92cc2c19d8657fd9914eb12701
said <96350462+saidjami@users.noreply.github.com>
2024-07-28 01:19:04 +0400

    Update main.py
    Simple band-name-generator for day 1.

--- Day-1 band-name-generator/main.py
+++ Day-1 band-name-generator/main.py
@@ -3,4 +3,4 @@ place = input("Write the name of your birthplace:")
 petname = input("Write the name of your first pet:")
 
 print("The name of your band: " + place + " " + petname)
-print("Nice name :)")
+print("Nice name :).")
