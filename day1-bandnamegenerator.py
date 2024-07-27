d8834aa0b8eb7a2c2fa1d20eebfc75f3877d188c
said <96350462+saidjami@users.noreply.github.com>
2024-07-28 01:11:30 +0400

    Delete day1-bandnamegenerator.py

--- day1-bandnamegenerator.py
+++
@@ -1,6 +0,0 @@
-place = input("Write the name of your birthplace:")
-
-petname = input("Write the name of your first pet:")
-
-print("The name of your band: " + place + " " + petname)
-print("Nice name :).")
