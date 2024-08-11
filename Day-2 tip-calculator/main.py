85a48b229dea9d66c02e9a72d2590d723cba7144
Said <96350462+oncesa1d@users.noreply.github.com>
2024-08-11 22:54:31 +0400

    Delete Day-2 tip-calculator/main.py

--- Day-2 tip-calculator/main.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? (Please do not add any percentage marks.)"))
-people = int(input("How many people to split the bill?"))
-bill_with_tip = bill / 100 * bill + bill
-bill_per_person = bill_with_tip / people
-final_amount = round(bill_per_person, 2)
-print(f"Each person should pay {final_amount}")
