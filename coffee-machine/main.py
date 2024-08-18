7fe369ac2a29afa3dba45a74d78d7b2b3a6555d5
Said <96350462+oncesa1d@users.noreply.github.com>
2024-08-18 22:05:30 +0400

    Update main.py

--- coffee-machine/main.py
+++ coffee-machine/main.py
@@ -1,5 +1,31 @@
-from menu import MENU
 from art import logo
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
 
 profit = 0
 resources = {
